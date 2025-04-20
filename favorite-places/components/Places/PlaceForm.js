58e81abbf78efc0758ae300fb18983930589ff95
Sai <sailakshmy94@gmail.com>
2025-04-20 11:34:35 +0400

    Created the AddPlace and the PlaceForm components

---
+++ favorite-places/components/Places/PlaceForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View } from "react-native";
+
+const PlaceForm = () => {
+  return (
+    <View>
+      <Text>The Place Form </Text>
+    </View>
+  );
+};
+
+export default PlaceForm;
---
+++ favorite-places/screens/AddPlace.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+import PlaceForm from "../components/Places/PlaceForm";
+
+const AddPlace = () => {
+  return <PlaceForm />;
+};
+
+export default AddPlace;
