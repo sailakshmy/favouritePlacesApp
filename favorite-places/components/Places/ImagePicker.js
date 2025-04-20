2dd97624a198f8e5096c0d6fe05fe6575e3034fd
Sai <sailakshmy94@gmail.com>
2025-04-20 23:06:49 +0400

    Created an image preview section

--- favorite-places/components/Places/ImagePicker.js
+++ favorite-places/components/Places/ImagePicker.js
@@ -1,12 +1,14 @@
-import React from "react";
-import { Alert, Button, View } from "react-native";
+import React, { useState } from "react";
+import { Alert, Button, Image, View, Text, StyleSheet } from "react-native";
 import {
   launchCameraAsync,
   PermissionStatus,
   useCameraPermissions,
 } from "expo-image-picker";
+import { Colors } from "../../constants/colors";
 
 const ImagePicker = () => {
+  const [pickedImage, setPickedImage] = useState();
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -36,13 +38,36 @@ const ImagePicker = () => {
       quality: 0.5,
     });
     console.log("image", image);
+    setPickedImage(image?.assets?.[0]);
   };
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>
+        {pickedImage ? (
+          <Image style={styles.image} source={{ uri: pickedImage?.uri }} />
+        ) : (
+          <Text>No Image taken yet</Text>
+        )}
+      </View>
       <Button title="Take image" onPress={takeImage} />
     </View>
   );
 };
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
