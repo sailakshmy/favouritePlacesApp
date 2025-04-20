f0576f3dab3d3b70915a4114106d09cc2ee6469d
Sai <sailakshmy94@gmail.com>
2025-04-20 12:12:43 +0400

    Modified the Place form component

--- favorite-places/components/Places/PlaceForm.js
+++ favorite-places/components/Places/PlaceForm.js
@@ -1,12 +1,46 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../constants/colors";
 
 const PlaceForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  function changeTitle(enteredText) {
+    setEnteredTitle(enteredText);
+  }
   return (
-    <View>
-      <Text>The Place Form </Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          onChangeText={changeTitle}
+          value={enteredTitle}
+          style={styles.input}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
 export default PlaceForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    padding: 24,
+  },
+  label: {
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: Colors.primary500,
+  },
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100,
+  },
+});
